305164e89daefbcab4a2f1c90c861696d60c1567
Matthew-Kwan <matthew.k913@gmail.com>
2021-03-17 20:50:58 -0400

    fixed issue with hover activating on hidden tooltips

--- quickDefs.js
+++ quickDefs.js
@@ -1,13 +1,23 @@
-const tooltips = [].slice.call(document.querySelectorAll('.term-highlight'))
+const tooltips = [].slice.call(document.querySelectorAll('.term-tooltip'))
+const termDefs = [].slice.call(document.querySelectorAll('.term-highlight'))
+
+let i = 0
 
 tooltips.forEach(function(tooltip) {
+
+  const tooltipDef = termDefs[i]
   const tooltipSpan = tooltip.querySelector('.term-definition')
 
-  tooltip.onmouseenter = function(e) {
+  tooltipDef.onmouseenter = function(e) {
     tooltipSpan.style.opacity = 1
   }
 
-  tooltip.onmouseleave = function(e) {
+  tooltipDef.onmouseleave = function(e) {
     tooltipSpan.style.opacity = 0
   }
+
+  i = i + 1
+
 })
+
+console.log(termDefs)
