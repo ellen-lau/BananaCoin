ae2a7effce23ddc43a7757f3f05c6f5736730c0c
Ellen Lau <Ellen.lau@ibm.com>
2021-03-22 12:59:14 -0400

    CC statement

--- scripts/billingCycle.js
+++ scripts/billingCycle.js
@@ -10,6 +10,14 @@ balanceInfo.set('pay-min-balance', 'You paid the minimum balance on time so your
 
 balanceInfo.set('pay-no-payment', 'You didn’t pay the minimum payment on time, your account isn’t in good standing anymore!<br><br>This means a few things:<br><br> - your credit card may be locked<br>- your interest rates may increase<br>- your credit score will go down')
 
+const balanceAmounts = new Map();
+
+balanceAmounts.set('pay-account-balance', '$0');
+balanceAmounts.set('pay-statement-balance', '$55');
+balanceAmounts.set('pay-min-balance', '$90')
+balanceAmounts.set('pay-no-payment', '$100')
+
+
 var firstClick = true;
 
 function payBalance(id) {
@@ -18,10 +26,18 @@ function payBalance(id) {
         for (var i = 0; i < balanceIds.length; i++) {
             document.getElementById(balanceIds[i]).classList.remove("paymentButtonFocused")
         }
+        document.getElementById("billing-cycle-interest-owed").classList.remove("amountsInterest");
+        document.getElementById("billing-cycle-interest-owed").innerHTML = '$10';
     }
 
     document.getElementById(id).classList.add("paymentButtonFocused")
     document.getElementsByClassName("billingCycleInfoBox")[0].innerHTML = balanceInfo.get(id)
+    document.getElementById("billing-cycle-account-balance").innerHTML = balanceAmounts.get(id)
+
+    if (id === 'pay-no-payment') {
+        document.getElementById("billing-cycle-interest-owed").classList.add("amountsInterest");
+        document.getElementById("billing-cycle-interest-owed").innerHTML = '$15';
+    }
 
     firstClick = false;
 }
